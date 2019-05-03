722badff5b4ea1ac4cf0acef0d53dfcd2f2d6d96
voknelim <voknelim@gmail.com>
2019-05-04 01:11:08 +0200

    expense fragment

--- app/src/main/java/ch/heig/cashflow/activites/MainActivity.java
+++ app/src/main/java/ch/heig/cashflow/activites/MainActivity.java
@@ -8,9 +8,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
 import ch.heig.cashflow.R;
+import ch.heig.cashflow.models.Earning;
+import ch.heig.cashflow.models.Expense;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private static final int REQUEST_ID_READ_PERMISSION = 100;
+    private static final int REQUEST_ID_WRITE_PERMISSION = 200;
+
+    public static final DecimalFormat df = new DecimalFormat("#.00");
+
+    public ArrayList<Earning> earningsArrayList = null;
+    public ArrayList<Expense> expensesArrayList = null;
+
+    private float earnings = 0.f;
+    private float expenses = 0.f;
 
     private TextView mTextMessage;
 
@@ -44,9 +61,12 @@ public class MainActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
 
-
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+    }
+
+    public String getExpenses() {
+        return df.format(expenses);
     }
 
 }
