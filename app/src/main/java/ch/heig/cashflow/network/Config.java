9b45dccbfbf627a2eb9427261ba939d9ec67cc0c
Thibaud <thibaudalt@users.noreply.github.com>
2019-05-04 13:58:31 +0200

    Update Config.java

--- app/src/main/java/ch/heig/cashflow/network/Config.java
+++ app/src/main/java/ch/heig/cashflow/network/Config.java
@@ -1,6 +1,17 @@
 package ch.heig.cashflow.network;
 
 public final class Config {
+
     protected final static String BASE_URL = "192.168.1.148:5000/api";
-    protected final static String LOGIN = BASE_URL + "/auth/signin";
+
+    protected final static String AUTH = BASE_URL + "/auth";
+    protected final static String AUTH_SIGNIN = BASE_URL + AUTH + "/signin";
+    protected final static String AUTH_SIGNUP = BASE_URL + AUTH + "/signup";
+
+    protected final static String TRANSACTIONS = BASE_URL + "/transactions";
+    protected final static String TRANSACTIONS_TYPE = BASE_URL + TRANSACTIONS + "/type";
+
+    protected final static String CATEGORIES = BASE_URL + "/categories";
+    protected final static String CATEGORIES_TYPE = BASE_URL + CATEGORIES + "/type";
+
 }
