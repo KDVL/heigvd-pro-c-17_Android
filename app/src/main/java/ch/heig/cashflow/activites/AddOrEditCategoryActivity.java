1cea0ec1918f7347b34c1d365ee92fab6ed14d25
voknelim <voknelim@gmail.com>
2019-05-13 00:41:32 +0200

    bub-fix

---
+++ app/src/main/java/ch/heig/cashflow/activites/AddOrEditCategoryActivity.java
@@ -0,0 +1,15 @@
+package ch.heig.cashflow.activites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ch.heig.cashflow.R;
+
+public class AddOrEditCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_or_edit_category);
+    }
+}
---
+++ app/src/main/java/ch/heig/cashflow/activites/CategoryDetailsActivity.java
@@ -0,0 +1,15 @@
+package ch.heig.cashflow.activites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ch.heig.cashflow.R;
+
+public class CategoryDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_details);
+    }
+}
